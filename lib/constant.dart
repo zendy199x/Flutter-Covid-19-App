396ff8c267bfc1f145893a94e25dd1fe611c171a
Zendy <nguyenduc.051194@gmail.com>
2021-07-30 12:02:06 +0700

    feat: add type constants file

--- lib/constant.dart
+++ lib/constant.dart
@@ -1,16 +1,16 @@
 import 'package:flutter/material.dart';
 
 // Colors
-const kBackgroundColor = Color(0xFFFEFEFE);
-const kTitleTextColor = Color(0xFF303030);
-const kBodyTextColor = Color(0xFF4B4B4B);
-const kTextLightColor = Color(0xFF959595);
-const kInfectedColor = Color(0xFFFF8748);
-const kDeathColor = Color(0xFFFF4848);
-const kRecovercolor = Color(0xFF36C12C);
-const kPrimaryColor = Color(0xFF3382CC);
-final kShadowColor = const Color(0xFFB7B7B7).withOpacity(0.16);
-final kActiveShadowColor = const Color(0xFF4056C6).withOpacity(0.15);
+const Color kBackgroundColor = Color(0xFFFEFEFE);
+const Color kTitleTextColor = Color(0xFF303030);
+const Color kBodyTextColor = Color(0xFF4B4B4B);
+const Color kTextLightColor = Color(0xFF959595);
+const Color kInfectedColor = Color(0xFFFF8748);
+const Color kDeathColor = Color(0xFFFF4848);
+const Color kRecovercolor = Color(0xFF36C12C);
+const Color kPrimaryColor = Color(0xFF3382CC);
+final Color kShadowColor = const Color(0xFFB7B7B7).withOpacity(0.16);
+final Color kActiveShadowColor = const Color(0xFF4056C6).withOpacity(0.15);
 
 // Text Style
 const TextStyle kHeadingTextStyle = TextStyle(
