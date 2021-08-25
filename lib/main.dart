bba381b30ac51b28fb700832377ba919e229a64c
Mohammad Ebrahimi <moh.ebrahimi2000@gmail.com>
2021-08-25 19:40:03 +0430

    Add SingleChildScrollView to main screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:covid_19/constant.dart';
-import 'package:covid_19/info_screen.dart';
 import 'package:covid_19/widgets/counter.dart';
 import 'package:covid_19/widgets/my_header.dart';
 import 'package:flutter/material.dart';
@@ -37,164 +36,166 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          const MyHeader(
-            image: "assets/icons/Drcorona.svg",
-            textTop: "All you need",
-            textBottom: "is stay at home.",
-          ),
-          Container(
-            margin: const EdgeInsets.symmetric(
-              horizontal: 20,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const MyHeader(
+              image: "assets/icons/Drcorona.svg",
+              textTop: "All you need",
+              textBottom: "is stay at home.",
             ),
-            padding: const EdgeInsets.symmetric(
-              vertical: 10,
-              horizontal: 20,
-            ),
-            height: 60,
-            width: double.infinity,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(25),
-              border: Border.all(
-                color: const Color(0xFFE5E5E5),
+            Container(
+              margin: const EdgeInsets.symmetric(
+                horizontal: 20,
+              ),
+              padding: const EdgeInsets.symmetric(
+                vertical: 10,
+                horizontal: 20,
+              ),
+              height: 60,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(25),
+                border: Border.all(
+                  color: const Color(0xFFE5E5E5),
+                ),
+              ),
+              child: Row(
+                children: [
+                  SvgPicture.asset("assets/icons/maps-and-flags.svg"),
+                  const SizedBox(width: 20),
+                  Expanded(
+                    child: DropdownButton(
+                      isExpanded: true,
+                      underline: const SizedBox(),
+                      icon: SvgPicture.asset("assets/icons/dropdown.svg"),
+                      value: "Vietnam",
+                      items: ["Vietnam", "Japan", "United State", "China"]
+                          .map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        );
+                      }).toList(),
+                      onChanged: (value) {},
+                    ),
+                  ),
+                ],
               ),
             ),
-            child: Row(
-              children: [
-                SvgPicture.asset("assets/icons/maps-and-flags.svg"),
-                const SizedBox(width: 20),
-                Expanded(
-                  child: DropdownButton(
-                    isExpanded: true,
-                    underline: const SizedBox(),
-                    icon: SvgPicture.asset("assets/icons/dropdown.svg"),
-                    value: "Vietnam",
-                    items: ["Vietnam", "Japan", "United State", "China"]
-                        .map<DropdownMenuItem<String>>((String value) {
-                      return DropdownMenuItem<String>(
-                        value: value,
-                        child: Text(value),
-                      );
-                    }).toList(),
-                    onChanged: (value) {},
-                  ),
-                ),
-              ],
-            ),
-          ),
-          const SizedBox(height: 20),
-          Padding(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 20,
-            ),
-            child: Column(
-              children: [
-                Row(
-                  children: [
-                    RichText(
-                      text: const TextSpan(
-                        children: [
-                          TextSpan(
-                            text: "Case Update\n",
-                            style: kTitleTextstyle,
-                          ),
-                          TextSpan(
-                            text: "Newest update July 27",
-                            style: TextStyle(
-                              color: kTextLightColor,
+            const SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 20,
+              ),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      RichText(
+                        text: const TextSpan(
+                          children: [
+                            TextSpan(
+                              text: "Case Update\n",
+                              style: kTitleTextstyle,
                             ),
-                          ),
-                        ],
+                            TextSpan(
+                              text: "Newest update July 27",
+                              style: TextStyle(
+                                color: kTextLightColor,
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
-                    ),
-                    const Spacer(),
-                    const Text(
-                      "See details",
-                      style: TextStyle(
-                        color: kPrimaryColor,
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 20),
-                Container(
-                  padding: const EdgeInsets.all(20),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    color: Colors.white,
-                    boxShadow: [
-                      BoxShadow(
-                        offset: const Offset(0, 4),
-                        blurRadius: 30,
-                        color: kShadowColor,
+                      const Spacer(),
+                      const Text(
+                        "See details",
+                        style: TextStyle(
+                          color: kPrimaryColor,
+                          fontWeight: FontWeight.w600,
+                        ),
                       ),
                     ],
                   ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  const SizedBox(height: 20),
+                  Container(
+                    padding: const EdgeInsets.all(20),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(
+                          offset: const Offset(0, 4),
+                          blurRadius: 30,
+                          color: kShadowColor,
+                        ),
+                      ],
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: const [
+                        Counter(
+                          color: kInfectedColor,
+                          number: 6318,
+                          title: "Intected",
+                        ),
+                        Counter(
+                          color: kDeathColor,
+                          number: 5,
+                          title: "Deaths",
+                        ),
+                        Counter(
+                          color: kRecovercolor,
+                          number: 1098,
+                          title: "Recovered",
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  Row(
                     children: const [
-                      Counter(
-                        color: kInfectedColor,
-                        number: 6318,
-                        title: "Intected",
+                      Text(
+                        "Spread of Virus",
+                        style: kTitleTextstyle,
                       ),
-                      Counter(
-                        color: kDeathColor,
-                        number: 5,
-                        title: "Deaths",
-                      ),
-                      Counter(
-                        color: kRecovercolor,
-                        number: 1098,
-                        title: "Recovered",
-                      ),
-                    ],
-                  ),
-                ),
-                const SizedBox(height: 20),
-                Row(
-                  children: const [
-                    Text(
-                      "Spread of Virus",
-                      style: kTitleTextstyle,
-                    ),
-                    Spacer(),
-                    Text(
-                      "See details",
-                      style: TextStyle(
-                        color: kPrimaryColor,
-                        fontWeight: FontWeight.w600,
-                      ),
-                    )
-                  ],
-                ),
-                Container(
-                  margin: const EdgeInsets.only(top: 20),
-                  height: 178,
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    color: Colors.white,
-                    boxShadow: [
-                      BoxShadow(
-                        offset: const Offset(0, 10),
-                        blurRadius: 30,
-                        color: kShadowColor,
+                      Spacer(),
+                      Text(
+                        "See details",
+                        style: TextStyle(
+                          color: kPrimaryColor,
+                          fontWeight: FontWeight.w600,
+                        ),
                       )
                     ],
                   ),
-                  child: Image.asset(
-                    "assets/images/map.png",
-                    fit: BoxFit.contain,
-                  ),
-                )
-              ],
+                  Container(
+                    margin: const EdgeInsets.only(top: 20),
+                    height: 178,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(
+                          offset: const Offset(0, 10),
+                          blurRadius: 30,
+                          color: kShadowColor,
+                        )
+                      ],
+                    ),
+                    child: Image.asset(
+                      "assets/images/map.png",
+                      fit: BoxFit.contain,
+                    ),
+                  )
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
